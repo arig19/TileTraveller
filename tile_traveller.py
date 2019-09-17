33241845ad68ef53e6744f5b76b07b89a853b1b5
ari <arig19@ru.is>
2019-09-17 09:39:18 +0000

    m

---
+++ tile_traveller.py
@@ -0,0 +1,4 @@
+# a computer game in which the player is located in a certain tile in
+#a grid. At each iteration, the program displays the directions for which there are adjacent tiles
+#that the player can travel to. 
+
